6213338ef1e53dc66bffdaa99abf35aa12a512d5
R-Kolbnev <RKolbnev@mail.ru>
2021-07-14 12:22:30 +0300

    Сделан дизайн страницы сообщений

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -19,8 +19,82 @@
     </div>
 
     <div class="chat">
+      <div class="chat-header">
+        <div class="person">
+          <div class="person__img">
+            <img src="../assets/test2.jpg" alt="">
+          </div>
+          <div class="person__name">
+            Ivanova Eva Ivanovna
+          </div>
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </div>
+      </div>
+      <div class="message__wrapper scroll">
+        <div class="message from">
+          <span>Hello</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message to">
+          <span>hi</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
 
+        <div class="message from">
+          <span>Hello</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message to">
+          <span>hi</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message from">
+          <span>Hello</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message to">
+          <span>hi</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message from">
+          <span>Hello</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+        <div class="message to">
+          <span>hi</span>
+          <div class="message__info">
+            <span>12 февраля</span>
+            <i class="fa fa-check " aria-hidden="true"></i>
+          </div>
+        </div>
+      </div>
+      <div class="chat-write">
+        <input type="text">
+        <button>Отправить</button>
+      </div>
     </div>
+
   </div>
 </template>
 
